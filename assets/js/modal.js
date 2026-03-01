0aecd40bb7397edcb2a9b46cfd54bb0a1b450bf6
Aluizio Catão <aluizio2210@gmail.com>
2026-02-28 20:28:05 -0500

    feat: Implement core site styling, introduce a new promotional modal, and integrate related images.

---
+++ assets/js/modal.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const modalElement = document.getElementById('modalInicial');
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+});
